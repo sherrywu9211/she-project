1f1bd9c0db76fc060fb64cb230ee62a426a3dc45
Sherry <l223971013@gmail.com>
2024-08-13 17:07:09 +0800

    add project

---
+++ src/main/java/idv/sherry/SherryProjectApplication.java
@@ -0,0 +1,13 @@
+package idv.sherry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SherryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SherryProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idv/sherry/SherryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.sherry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SherryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
